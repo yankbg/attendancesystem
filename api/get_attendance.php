91d0f0d2d3181a5f6dae07d0a99161154725c714
YANNICK kabunga <yankbg2@gmail.com>
2025-06-24 19:56:47 +0300

    Update get_attendance.php

--- api/get_attendance.php
+++ api/get_attendance.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 // Handle preflight OPTIONS request
