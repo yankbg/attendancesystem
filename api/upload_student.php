29c4811fee399ca44b085e4d8f12564b3e968daa
YANNICK kabunga <yankbg2@gmail.com>
2025-06-24 19:59:52 +0300

    Update upload_student.php

--- api/upload_student.php
+++ api/upload_student.php
@@ -2,6 +2,14 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Load environment variables (you can use vlucas/phpdotenv or set in your environment)
 $cloudinaryUrl = getenv('CLOUDINARY_URL'); // e.g. cloudinary://API_KEY:API_SECRET@CLOUD_NAME
