d239ea4c15364ff7ce20983ab750cc84aa1a7eaf
YANNICK kabunga <yankbg2@gmail.com>
2025-06-24 19:55:50 +0300

    Update check_student.php

--- api/check_student.php
+++ api/check_student.php
@@ -1,7 +1,8 @@
 <?php
+
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 // Handle preflight OPTIONS request for CORS
